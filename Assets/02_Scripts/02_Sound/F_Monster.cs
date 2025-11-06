1589c062f5fad855f66fac479ba596f7d5c784ac
Daniel Goudel (1248394) <1248394@bimmstudents.com>
2025-11-06 18:13:42 +0100

    monster events wip

---
+++ Assets/02_Scripts/02_Sound/F_Monster.cs
@@ -0,0 +1,62 @@
+using System;
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class F_Monster : MonoBehaviour {
+    int materialValue;
+    RaycastHit rayHit;
+    float rayDistance = 0.3f;
+    const string EVENT_PATH1 = "event:/Character/DarkSteps";
+    const string EVENT_PATH2 = "event:/Character/MonsterSteps";
+
+    public float Volume = 4;
+
+    void OnEnable() {
+        EventManager.Subscribe("sfx-follow", PlayFollowEvent);
+        EventManager.Subscribe("sfx-heavy", PlayHeavyEvent);
+    }
+
+    void OnDisable() {
+        EventManager.Unsubscribe("sfx-follow", PlayFollowEvent);
+        EventManager.Unsubscribe("sfx-heavy", PlayHeavyEvent);
+    }
+
+
+    void PlayFollowEvent() {
+        MaterialCheck();
+        EventInstance follow = RuntimeManager.CreateInstance(EVENT_PATH1);
+        RuntimeManager.AttachInstanceToGameObject(follow, transform, true);
+
+        follow.setVolume(4);
+
+        follow.setParameterByName("Terrain", materialValue);
+        follow.setParameterByName("WalkRun", 1, false);
+        follow.start();
+        follow.release();
+    }
+    void PlayHeavyEvent() {
+        MaterialCheck();
+        EventInstance heavy = RuntimeManager.CreateInstance(EVENT_PATH2);
+        RuntimeManager.AttachInstanceToGameObject(heavy, transform, true);
+
+
+        heavy.setParameterByName("MonsterSteps", 0, false); // 0 is walk
+
+        heavy.start();
+        heavy.release();
+    }
+
+
+    void MaterialCheck() {
+        if (Physics.Raycast(transform.position, Vector3.down, out rayHit, rayDistance, LayerMask.GetMask("Ground"))) {
+            switch (rayHit.collider.tag) {
+                case "Grass": materialValue = 0; break;
+                case "Gravel": materialValue = 1; break;
+                case "Wood": materialValue = 2; break;
+                case "Cement": materialValue = 3; break;
+            }
+        }
+    }
+}
--- Assets/02_Scripts/Monster.cs
+++ Assets/02_Scripts/Monster.cs
@@ -1,9 +1,8 @@
-using System;
 using UnityEngine;
 
-public class Monster : MonoBehaviour
-{
-    [Range(0,100)]public float aggression = 0;
+public class Monster : MonoBehaviour {
+    [Header("Aggression")]
+    [Range(0, 100)] public float aggression = 0;
     [Tooltip("How fast aggression reduces normaly")]
     public float initialPacifyMult = 2f;
     [Tooltip("How fast aggression reduces when the player is quiet for long enough")]
@@ -13,51 +12,82 @@ public class Monster : MonoBehaviour
     public float quietTimerLimit = 5;
     float quietTimer;
 
-    public float monsterDistance = 15f;
 
+    [Header("Thresholds")]
+    [Tooltip("Monster starts following behind you")]
+    int threshold1 = 25;
+    [Tooltip("Ambience randomly mutes")]
+    int threshold2;
+    [Tooltip("Spawn eyes out of view")]
+    int threshold3 = 85;
+
+    bool threshold1Hit; //following footsteps
+    bool threshold2Hit;
+    bool threshold3Hit;
+
+    [Header("Monster Events")]
+    public float monsterBaseDistance = 15f;
+    [SerializeField] float followTimerLimit = 0.6f;
+    [SerializeField] float followTimer;
+
+
+    GameObject player;
     RaycastHit rayHit;
     float rayDistance = Mathf.Infinity;
 
-    bool threshold1; //following footsteps
-    bool threshold2; 
-    bool threshold3;
-
-    void OnEnable() {
-        EventManager.SubscribeFloat("add-noise", AddAgression);
-    }
-    void OnDisable() {
-        EventManager.UnsubscribeFloat("add-noise", AddAgression);
-    }
+    void OnEnable() => EventManager.SubscribeFloat("add-noise", AddAgression);
+    void OnDisable() => EventManager.UnsubscribeFloat("add-noise", AddAgression);
+    void Start() => player = GameObject.Find("Player");
 
     void Update() {
-        if(!threshold1 && aggression > 0) threshold1 = true;
+        if (!threshold1Hit && aggression > threshold1) threshold1Hit = true;
+        if (!threshold2Hit && aggression > threshold2) threshold2Hit = true;
+        if (!threshold3Hit && aggression > threshold3) { 
+            threshold3Hit = true; 
+            TPOutOfView(); 
+        }
 
         if (quietTimer > quietTimerLimit) pacifyMultiplier = quietPacifyMult;
         else pacifyMultiplier = initialPacifyMult;
 
+        if (followTimer < followTimerLimit) followTimer += Time.deltaTime;
 
         aggression -= pacifyMultiplier * Time.deltaTime;
     }
 
     void FixedUpdate() {
-        if (true) StartFollowing();
+        if (threshold1Hit) StartFollowing();
     }
+
 
     void AddAgression(float amount) {
         aggression += amount;
         quietTimer = 0;
     }
-    
-    void StartFollowing() {
-        GameObject player = GameObject.Find("Player");
-        transform.position = player.transform.position + player.transform.forward * monsterDistance;
 
-        if (Physics.Raycast(transform.position + new Vector3(0,10,0), Vector3.down, out rayHit, rayDistance, LayerMask.GetMask("Ground"))) {
+    void StartFollowing() {
+        if (threshold3Hit) return;
+        transform.position = player.transform.position - player.transform.forward * monsterBaseDistance;
+
+        if (Physics.Raycast(transform.position + new Vector3(0, 10, 0), Vector3.down, out rayHit, rayDistance, LayerMask.GetMask("Ground"))) {
             transform.position = new(transform.position.x, rayHit.point.y, transform.position.z);
         }
 
-        //add footsteps
-        //start at random intervals
+        if (followTimer > followTimerLimit) {
+            EventManager.Trigger("sfx-follow");
+            followTimer = 0;
+        }//start at random intervals
     }
 
+    void TPOutOfView() {
+        var pt = player.transform;
+        //spawn on either the left or the right of the player
+        transform.position = Random.Range(0, 1) == 0 ? pt.position + pt.right * monsterBaseDistance : pt.position - pt.right * monsterBaseDistance;
+
+        if (Physics.Raycast(transform.position + new Vector3(0, 10, 0), Vector3.down, out rayHit, rayDistance, LayerMask.GetMask("Ground"))) {
+            transform.position = new(transform.position.x, rayHit.point.y + 2f, transform.position.z);
+        }
+
+        EventManager.Trigger("sfx-heavy");
+    }
 }
